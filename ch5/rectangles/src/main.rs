ad6c50aaeaa07f747e6ef9ae0f0943df1add1c31
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-02-26 12:07:03 -0500

    2-26

--- ch5/rectangles/src/main.rs
+++ ch5/rectangles/src/main.rs
@@ -1,20 +1,32 @@
 
+#[derive(Debug)]
 struct Rectangle {
-    width = u32,
-    height = u32,
+    width: u32,
+    height: u32,
 }
 
 fn main() {
+
+    let scale = 2;
+    let rect2 = Rectangle{
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect2);
+
     let rect1 = Rectangle{
         width: 30,
         height: 50
     };
 
-    println!("The area of the rectangle is {} pixels", area(width1, height1));   
+    println!("rect1 is {rect1:#?}");
+
+    println!("The area of the rectangle is {} pixels", area(&rect1));   
     
 }
 
 
-fn area(rectangle:) -> u32{
-    width * height
+fn area(rectangle: &Rectangle) -> u32{
+    rectangle.width * rectangle.height
 }
