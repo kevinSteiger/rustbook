d28d2f189a4fee2ceb3992c083652afd5a70555b
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-04-12 14:55:17 -0400

    uber basic hashmap

--- random/hash_map/src/main.rs
+++ random/hash_map/src/main.rs
@@ -1,3 +1,7 @@
+//Ridiculously basic hashmap implementation
+//doesn't handle collisions, and can only store 10 values, cannot handle negative values
+
+
 
 fn insert_hash(arr: &mut [i32], value: i32){
 
@@ -6,9 +10,29 @@ fn insert_hash(arr: &mut [i32], value: i32){
 
 } 
 
+fn remove_hash(arr: &mut [i32], value: i32){
+    let index = (value % 10) as usize;
+    arr[index] = -1;
+}
+
+fn get_element(arr: &[i32], value: i32) -> i32 {
+    let index = (value % 10) as usize;
+    arr[index]
+}
 
 fn main() {
-    let mut hm = [0; 10];
+    let mut hm = [-1; 10];
+
+    println!("{:?}", hm);
+
+    insert_hash(&mut hm, 1);
+    insert_hash(&mut hm, 7);
+
+    println!("{:?}", hm);
+
+    let test = get_element(&hm, 17);
+
+    println!("{}", test);
 
 
 }
