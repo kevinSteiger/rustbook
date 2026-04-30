efd4f72315418f0dea6b91212cc9f7adcba6631f
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-04-30 11:56:16 -0400

    4-30

--- ch7/restaurant/src/lib.rs
+++ ch7/restaurant/src/lib.rs
@@ -15,18 +15,46 @@ mod front_of_house {
     }
 }
 
-fn deliver order() {}
+fn deliver_order() {}
 
 mod back_of_house{
-    
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast{
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast{
+                toast: String::from(toast),
+                seasonal_fruit: String::from("Peaches"),
+            }
+        }
+    }
+
     fn fix_incorrect_order(){
         cook_order();
         super::deliver_order();
     }
 
-    fn cook_order();
+    fn cook_order() {}
 }
 pub fn eat_at_restaurant() {
+
+    let order1 = back_of_house::Appetizer::Soup;
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast", meal.toast);
+
+
+
     crate::front_of_house::hosting::add_to_waitlist();
 
     front_of_house::hosting::add_to_waitlist();
