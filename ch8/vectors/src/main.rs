44d923f9e7c02d6f69efdaa4fbb7950ad07de19c
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-05-18 15:30:31 -0400

    5-18 3

--- ch8/vectors/src/main.rs
+++ ch8/vectors/src/main.rs
@@ -7,6 +7,21 @@ fn main() {
     v3.push(6);
     v3.push(7);
     v3.push(8);
-    
-    println!("Hello, world!");
+
+    let mut v4 = vec![1,2,3,4,5];
+    let third: &i32 = &v4[2];
+    println!("The third element is: {}", third);
+
+
+    let third: Option<&i32> = v4.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There's no third element"),
+    }
+
+
+    let first = &v4[0];
+    println!("The first element is: {first}");
+    v4.push(6);
+
 }
