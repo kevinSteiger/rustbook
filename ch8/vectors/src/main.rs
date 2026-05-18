c1a975438ab194275a6dcc4e208efeb8f0f20758
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-05-18 00:44:33 -0400

    5-18 v2

---
+++ ch8/vectors/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let v1: Vec<i32> = Vec::new();
+    let v2 = vec![1, 2, 3];
+
+    let mut v3 = Vec::new();
+    v3.push(5);
+    v3.push(6);
+    v3.push(7);
+    v3.push(8);
+    
+    println!("Hello, world!");
+}
