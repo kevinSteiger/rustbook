67fdc1427f1eb9ad9ae23afbb0108159d48abd40
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-07-31 11:23:07 -0400

    7-31

--- ch9/r_errors/src/main.rs
+++ ch9/r_errors/src/main.rs
@@ -1,18 +1,7 @@
 use std::fs::File;
 
 fn main() {
-    let greeting_file_result = File::open("hello.txt");
+    let greeting_file_result = File::open("hello.txt").expect("hello.txt should be included in this project");
 
-    let greeting_file = match greeting_file_result {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt"){
-                Ok(fc) => fc,
-                Err(e) => panic!("Problem creating the file: {e:?}"),
-            },
-            _ => {
-                panic!("Problem opening the file: {error:?}"),
-            }
-        }, 
-    };
+
 }
