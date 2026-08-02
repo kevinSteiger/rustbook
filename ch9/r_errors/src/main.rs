2008686d5dfa8fd38047ab7f90491fa2b0607e0f
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-08-02 13:57:14 -0400

    8-2 s1 & s2

--- ch9/r_errors/src/main.rs
+++ ch9/r_errors/src/main.rs
@@ -1,4 +1,12 @@
 use std::fs::File;
+use std::io::{self, Read}
+
+fn read_username_from_file() -> Result<String, io::Error>{
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
 
 fn main() {
     let greeting_file_result = File::open("hello.txt").expect("hello.txt should be included in this project");
