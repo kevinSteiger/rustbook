6c7ff73540b292679a313ca8d14cc0c045bd8fb9
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-08-03 00:55:41 -0400

    8-3 s1

--- ch9/r_errors/src/main.rs
+++ ch9/r_errors/src/main.rs
@@ -1,15 +1,18 @@
 use std::fs::File;
-use std::io::{self, Read}
+use std::fs;
+use std::io;
 
 fn read_username_from_file() -> Result<String, io::Error>{
-    let mut username_file = File::open("hello.txt")?;
-    let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
-    Ok(username)
+    fs::read_to_string("hello.txt")
+}
+
+fn last_char_of_first_line(text: &str) -> Option<char>{
+    text.lines().next()?.chars().last()
 }
 
 fn main() {
     let greeting_file_result = File::open("hello.txt").expect("hello.txt should be included in this project");
 
+    let greeting_file = File::open("hello.txt")?;
 
 }
