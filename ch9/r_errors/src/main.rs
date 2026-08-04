faa87cbd1cf74e491e4d43cbe7968b0e9e5998d6
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-08-04 00:36:07 -0400

    8-4 s1

--- ch9/r_errors/src/main.rs
+++ ch9/r_errors/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::fs;
 use std::io;
+use std::error::Error;
 
 fn read_username_from_file() -> Result<String, io::Error>{
     fs::read_to_string("hello.txt")
@@ -10,9 +11,9 @@ fn last_char_of_first_line(text: &str) -> Option<char>{
     text.lines().next()?.chars().last()
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>>{
     let greeting_file_result = File::open("hello.txt").expect("hello.txt should be included in this project");
 
     let greeting_file = File::open("hello.txt")?;
-
+    Ok(())
 }
