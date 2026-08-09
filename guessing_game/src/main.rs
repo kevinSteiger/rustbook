5e5d9e7b73461afe7713066ed747d9934427b65c
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-08-09 15:05:34 -0400

    8-9 s1 s2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,6 +2,25 @@ use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
+pub struct Guess{
+    value: i32,
+}
+
+impl Guess{
+    pub fn new(value: i32) -> Guess{
+        if value < 1 || value > 100{
+            panic!("Guess value must be between 1 amd 100, got {value}.");
+        }
+
+        Guess{value}
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
+
 fn main() {
     println!("Guess the number!");
 
@@ -17,14 +36,16 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        let guess: Guess = Guess::new(guess);
 
-        match guess.cmp(&secret_number) {
+        println!("You guessed: {}", guess.value);
+
+        match guess.value.cmp(&secret_number) {
             Ordering::Less => println!("Too Small"),
             Ordering::Greater => println!("Too Big"),
             Ordering::Equal => {println!("You win!");
