01e887ea056e5af8e6cf97f02631dbcb7683c3ce
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-08-23 13:06:14 -0400

    8-23 s3

--- ch10/generics/src/main.rs
+++ ch10/generics/src/main.rs
@@ -1,5 +1,9 @@
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
 
-fn largest(list: &[i32]) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
     let mut largest = &list[0];
 
     for number in list{
@@ -11,14 +15,42 @@ fn largest(list: &[i32]) -> &i32 {
     largest
 }
 
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest{
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest{
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
-
     println!("The largest element is: {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {result}");
 
-    let result = largest(&number_list);
-    println!("The largest number is: {}", result);
+    let integer = Point {x: 5, y: 10};
+    let float = Point { x: 1.0, y: 4.0};
+    let mixed = Point {x: 1.0, y: 5};
+
 }
