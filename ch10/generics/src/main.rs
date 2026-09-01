3d6fee60787aac0fc2490e0998bfe6ae90b94c73
kevinSteiger <124078628+kevinSteiger@users.noreply.github.com>
2026-09-01 01:03:41 -0400

    9-1

--- ch10/generics/src/main.rs
+++ ch10/generics/src/main.rs
@@ -10,8 +10,27 @@ impl<T> Point<T> {
 }
 
 impl Point<f32> {
-    
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
 }
+
+
+struct Point_2<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point_2<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point_2<X2, Y2>) -> Point_2<X1, Y2> {
+        Point_2{
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+
 
 enum Fake_Result<T, E> {
     Ok(T),
@@ -71,4 +90,10 @@ fn main() {
     let p = Point { x: 5, y: 10};
     
     println!("p.x = {}", p.x());
+
+
+    let p = Point {x: 3.0, y: 4.1};
+    println!("distance from origin is: {}, starting at point {}, {}", p.distance_from_origin(), p.x, p.y);
+
+
 }
